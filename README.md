e0cd62e6140290f961e6d0cdf32d23a4742f7ee2
Ethan Adams <ethan.adams@live.ca>
2017-12-13 16:17:45 +0000

    added camera

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # face-track-cv
 using opencv
+
+useful command for opening the pi camera module as a usb interface:
+sudo modprobe bcm2835-v4l2
--- main.cpp
+++ main.cpp
@@ -2,10 +2,33 @@
 #include <opencv2/opencv.hpp>
 #include "gpio.h"
 
+using namespace cv;
 
+VideoCapture getCapture(int camNum)
+{
+	VideoCapture vcap;
+	
+	while(!vcap.open(camNum));
+
+	return vcap;
+}
 
 int main(int argc, char* args[])
 {
-	std::cout << "hello" << std::endl;
+	namedWindow("win", CV_WINDOW_AUTOSIZE);
+	VideoCapture vcap = getCapture(0);
+	Mat img;
+	while(vcap.read(img))
+	{
+		resize(img, img, Size(800, 600));
+		rotate(img, img, 1.0f);
+
+		imshow("win", img);
+		
+		// If escape is pressed we break;
+		if (waitKey(10) == 27)
+			break;			
+	}	
+
 	return 0;
 }
